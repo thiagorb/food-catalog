00bf999c4d404a665ee5ca2ca2399b44f977fc8a
Thiago Barcala <thiago.barcala@gmail.com>
2019-11-20 19:29:09 +0100

    Add compiled version

---
+++ dist/precache-manifest.0e944e8a467cfcd77d620e183d6d731c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7dc23b92ffb57c6f5a5a992095258638",
+    "url": "./index.html"
+  },
+  {
+    "revision": "442488b21ed73b908809",
+    "url": "./static/css/main.f14bf0a0.chunk.css"
+  },
+  {
+    "revision": "edad76ba39b5c0449262",
+    "url": "./static/js/2.9905bd49.chunk.js"
+  },
+  {
+    "revision": "442488b21ed73b908809",
+    "url": "./static/js/main.8e87075b.chunk.js"
+  },
+  {
+    "revision": "e1bbc01a8adb63d3b224",
+    "url": "./static/js/runtime-main.e3c122cc.js"
+  },
+  {
+    "revision": "6de4ffbe77ac0da141e0b1e332708bd3",
+    "url": "./static/media/tick.6de4ffbe.svg"
+  }
+]);
